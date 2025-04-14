998e305ee35c50b801c8f0d5f0d65e4e866aba82
PhamVanHung412004 <hp36780@gmail.com>
2025-04-15 02:27:04 +0700

    "Remove file word"

--- test.py
+++ test.py
@@ -1,16 +1,30 @@
-from sklearn.datasets import make_blobs
-import matplotlib.pyplot as plt
-import pandas as pd
-from Init_KMeans import Build_KMeans
-from labels_points import Convert_labels_points
-from pathlib import Path
+import re
+from docx import Document
+def extract_questions(text):
+    question_dict = {}
+    matches = re.split(r"(Câu\s+\d+[:\.])", text)
+    for i in range(1, len(matches), 2):
+        key = matches[i].strip().replace(":", "").replace(".", "")
+        content = matches[i+1].strip()
+        options = re.findall(r"([A-D])\.\s*(.*?)\s*(?=(?:[A-D]\.|$))", content, re.DOTALL)
+
+        if options:
+            question_text = re.split(r"[A-D]\.", content)[0].strip()
+            question_dict[key] = {
+                "question": question_text,
+                "options": {opt[0]: opt[1].strip() for opt in options}
+            }
+        else:
+            question_dict[key] = {
+                "question": content.strip(),
+                "options": None
+            }
+    return question_dict
+
+
 def main():
-    path = Path(__file__).parent / "test.json"
-
-    X, y_true = make_blobs(n_samples=300, centers=3, cluster_std=1.0, random_state=42)    
-    n_clusters = 3
-    labels = Build_KMeans(n_clusters,X).get_labels()
-    center_points = Build_KMeans(n_clusters,X).get_center_points()
-    data_test = Convert_labels_points(path,X,labels,n_clusters).save_file_json()
-
+    doc = Document("dataset/BUỔI 7 - LUYỆN TẬP DAO ĐỘNG ĐIỀU HOÀ.docx")
+    full_text = "\n".join([para.text for para in doc.paragraphs])
+    result = extract_questions(full_text)
+    print(result)
 main()
