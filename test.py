d24b3808e93549deebdb5f054b89fa59fee594fa
hai <kho>
2025-04-16 01:32:53 +0700

    đã sửa

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 from pathlib import Path
-from read_file import Read_File_WORD
+from read_file import Read_File_PDF
+from sentence_transformers import SentenceTransformer
+from Embedding_data import Get_Embedding
 def main():
-    path = Path(__file__).parent/"dataset/BUỔI 7 - LUYỆN TẬP DAO ĐỘNG ĐIỀU HOÀ.docx"
-    data = Read_File_WORD(path).Read()
-    print(data)
+    path = Path(__file__).parent/"dataset/ÔN TẬP GIỮA KÌ 2.pdf"
+    data = Read_File_PDF(path).Read()
+    model = SentenceTransformer('BAAI/bge-small-en-v1.5')
+    embedding = Get_Embedding(model,data).use_model()
+    
+
+
 main()
