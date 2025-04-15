3b226ffbfd162c31cc1e3ad2ff9c819064fad4d7
PhamVanHung412004 <hp36780@gmail.com>
2025-04-16 01:10:00 +0700

    fix sussfully package read_file

--- read_file/__init__.py
+++ read_file/__init__.py
@@ -17,29 +17,29 @@ class Get_Path:
         '''
         self.path = path
 
-class Read_File_CSV(Get_Path):
-    def __init__(self, path):
-        super().__init__(path)
-        '''
-        Kế thừa đường dẫn từ class Get_Path
-        '''
+# class Read_File_CSV(Get_Path):
+#     def __init__(self, path):
+#         super().__init__(path)
+#         '''
+#         Kế thừa đường dẫn từ class Get_Path
+#         '''
     
-    def Read(self)-> pd:
-        return pd.read_csv(self.path)
+#     def Read(self)-> pd:
+#         return pd.read_csv(self.path)
     
-class Read_File_PDF(Get_Path):
-    def __init__(self, path):
-        super().__init__(path)
-        '''
-        Kế thừa đường dẫn từ class Get_Path
-        '''
+# class Read_File_PDF(Get_Path):
+#     def __init__(self, path):
+#         super().__init__(path)
+#         '''
+#         Kế thừa đường dẫn từ class Get_Path
+#         '''
     
-    def Read(self):
-        reads = PdfReader(self.path)
-        texts = ""
-        for read in reads.pages:
-            texts += read.extract_text() or ''
-        return texts
+#     def Read(self):
+#         reads = PdfReader(self.path)
+#         texts = ""
+#         for read in reads.pages:
+#             texts += read.extract_text() or ''
+#         return texts
         
 
 class Read_File_WORD(Get_Path):
