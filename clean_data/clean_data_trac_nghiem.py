8d3e281cb405832de8917958ca0374b7abb0da24
PhamVanHung412004 <hp36780@gmail.com>
2025-04-27 22:06:47 +0700

    remove file

--- clean_data/clean_data_trac_nghiem.py
+++ clean_data/clean_data_trac_nghiem.py
@@ -12,41 +12,125 @@ from package import (
 from solution_string import (
     solution,
     clean_mathml,
-    clean_question,
+    clean_choice,
+    convert_dict,
+    check,
+    dict_to_text,
     fix_spacing_and_comma,
+    string_seach_character
 )
 
-def main():
-    datas = []
-    for i in range(11,13):
-        path = Path(__file__).parent/ "500_cau_hoi_trac_nghiem" / "500_trac_nghiem_{}.json".format(i)
-        texts = Read_File_Json(path).Read()
-        
-        for text in texts:
-            cauhoi, luachon = clean_question(text["cau_hoi"])
-            ts = text["dap_an"]
-            cleaned_ds = []
-            for t in ts:
-                left, right = solution(t)                        
-                
-                if (left != -1 and right != -1):
-                    cleaned_mathml = clean_mathml(t[left : right + 1])
-                    cleaned_mathml = fix_spacing_and_comma(cleaned_mathml)
-                    cleaned_ds.append(cleaned_mathml)
-                else:                
-                    cleaned_ds.append(t)
-            
-            text_answers = "\n".join([i for i in cleaned_ds if (i != "")])
-            selection = luachon.split("\n")
-            
-            if (len(selection) != 1):                        
-                ds = {}
-                ds["title"] = "trac nghiem"
-                ds["cau_hoi"] = cauhoi
-                ds["cac_lua_chon"] = selection
-                ds["giai_thich"] = text_answers
-                datas.append(ds)
-    path_save = Path(__file__).parent/ "data_fine_turning" / "data_fine_turning.json"
-    Save_File_Json(str(path_save),datas).save()    
+def remove_string(string_after : str, string_before : str) -> str:
+    '''
+    string_after : xau khi tu sau khi lam sach la cong thuc toan
+    string_before : xau khi tu truoc khi chua lam sach
 
+    '''
+    print(string_after)
+    # ki tu dau tien sau khi lam sach
+    char_begin = string_after[0]
+    index = -1
+    for i in range(len(string_before)):
+        if (string_before[i] == char_begin):
+            index = i
+            break
+    return string_before[: index]
+def main():    
+
+    cleaned_data = []
+    cnt = 0
+    for i in range(11,13):
+        path_json = Path(__file__).parent / "500_cau_hoi_trac_nghiem" / "500_trac_nghiem_{}.json".format(i)
+        datas = Read_File_Json(path_json).Read()  
+        print("file json lop: {}".format(i))
+        for data in datas:
+            datas_new = {}
+            question = data["cau_hoi"]
+            question = question.split("\n")
+
+            index = -1
+            for i in range(len(question[0])):
+                if (question[0][i] == '.' or question[0][i] == ':'):
+                    index = i
+                    break
+
+            question[0] = question[0][index + 2: ]
+
+            # kiem tra xem co dau ? o cuoi cau hay khong neu khong co thi them dau hoi vao ?
+
+            '''
+            khi ma so luong danh sach cau hoi sau 
+            khi chia bang 1 thi tuc la khong co lua chon
+            ta quy do la thuoc title bai tap
+            '''
+            if (len(question) == 1):                
+                datas_new["title"] = "bai tap"
+                datas_new["cau_hoi"] = question[0]    
+                text_news = [clean_mathml(text[solution(text)[0] : solution(text)[1] + 1]) if (solution(text) != (-1,-1)) else text for text in texts]                                          
+                datas_new["giai_thich"] = "\n".join(text_news)   
+
+            # '''
+            # khi ma so luong danh sach tu 2 do len 
+            # tuc la co 1 cau va co dap an lua chon
+            # ta quy ve do la cau hoi trac nghiem
+            # '''
+            else:                            
+                datas_new["title"] = "trac nghiem"
+                datas_new["cau_hoi"] = question[0]
+                selection_choice = question[1 : ]    
+                options = clean_choice(selection_choice)
+
+                if (len(options) == 0):
+                    options = convert_dict(selection_choice)
+                else:
+                    options = check(options)
+
+                dict_to_list = dict_to_text(options)
+
+                list_answers = data["dap_an"]
+                
+                vector_text = []
+                for i in range(len(list_answers)):
+                    left, right = solution(list_answers[i])
+                    if (left != -1 and right != -1):
+                        char_begin = list_answers[0]
+                        char_end = list_answers[-1]
+                        if (list_answers[i] == list_answers[i][left : right + 1]):
+                            vector_text.append(list_answers[i])
+                        else:
+                            print("left:",left , "right:", right)
+                            string_end = clean_mathml(list_answers[i][left : right + 1])
+                            # print("string clean:",fix_spacing_and_comma(string_end))
+
+                            string_begin = string_seach_character(string_end,list_answers[i][left : right + 1])
+                            print("string begin:",string_begin)
+                            # print(string_begin + string_end)
+                            # string_beautiful = char_math_begin + char_math_last
+                        # vector_text.append(string_beautiful)
+                    else:    
+                        print("string before:",list_answers[i])
+                #         vector_text.append(list_answers[i])
+                # text_beautiful = "\n".join(vector_text)
+
+            #     datas_new["cac_lua_chon"] = dict_to_list
+            #     datas_new["giai_thich"] = text_beautiful
+            # cleaned_data.append(datas_new)
+    
+        # check_oke = False
+        # for i in range(len(question[0])):
+        #     if (question[0][i] == '?'):
+        #         check_oke = True 
+        #         break    
+        # if (not check_oke):
+        #     question[0] += '?'
+
+        # cleaned_data.append({
+        #     "title" : "trac nghiem",
+        #     "cau_hoi": question,
+        #     "cac_lua_chon": result,
+        #     "giai_thich": explanation
+        # })
+        # print("Cau thu: {}".format())
+            # cnt += 1
+        print(cleaned_data)
 main()
--- clean_data/solution_string.py
+++ clean_data/solution_string.py
@@ -3,74 +3,120 @@ add()
 
 from package import (
     re,
-    BeautifulSoup
+    BeautifulSoup,
+    Dict
 )
 
-# def clean_question(raw_text : str) -> tuple:
-#     # Chuẩn hóa input
-#     raw_text = raw_text.strip()
+def char_test(char : str):
+    '''
+    char : kí tự muốn kiểm tra
+    '''
+    return 65 <= ord(char) and ord(char) <= 65 + 32
+
+
+def check_char(vector_answer : list[str]) -> bool:
+    '''
+    vector_answer : chứa danh sách các câu trả lời
+    '''
+    cnt = 0
+    for i in range(len(vector_answer[1 : ])):
+        char = vector_answer[i][0]
+        if (char_test(char)):
+            cnt += 1
+            
+    return True if (cnt >= 2 and cnt <= 4) else False
+
+def check_char_index(vector_answer : list[str], char : str) -> str:
+    '''
+    vector_answer : chứa danh sách các câu trả lời
+    char : kí tự đáp án đúng của câu đấy
+    '''
+    index = -1
+    for i in range(len(vector_answer)):
+        if (char_test(vector_answer[i][0])):
+            if (vector_answer[i][0] == char):
+                index = i
+                break
+    return vector_answer[index]
+
+def get_char(text : str) -> str:    
+    '''
+    text : từng đoạn văn bản trong danh sách các câu trả lời
+    '''
+    for i in range(len(text) - 1, -1, -1):
+        if (char_test(text[i])):
+            return i
+        
+def index_char_true(vector : list) -> str:
+    '''
+    vector : chứa các danh sach câu trả lời
+    '''
+    index = -1    
+    for i in range(len(vector)):
+        if ("đúng" in vector[i][1 : ] or "Đúng" in vector[i][1 : ]):
+                index = i
+                break       
+    return vector[index]
+
+
+def convert_dict(options_raw : Dict[str , str]) -> Dict[str , str]:
+    '''
+    options_raw : gom dap an va noi dung dap an
+    '''
+    options = {}
+    for line in options_raw:
+        match = re.match(r"([A-D])\.\s*(.*)", line)
+        if match:
+            options[match.group(1)] = match.group(2).strip()
+
+    return options
+
+# kiểm tra xem kí tự in hoa tìm được có hợp lệ hay không
+def range_char(char : str) -> bool:
+    '''
+    char : ki tu cua dap an dung
+    '''
+    table_char = ['A', 'B', 'C', 'D']    
+    return char in table_char
+
+def check_value(string : str) -> tuple:
+    index = -1
+    oke = False
+    for i in range(len(string)):
+        if (char_test(string[i])):
+            index = i 
+            oke = True
+            break                             
+    return (oke,index)
+
+def check(options : Dict[str , str]) -> Dict[str , str]:
+    '''
+    options : gom dap an va noi dung cua dap an
+    '''
+    dict_new = {}
+    for key, value in options.items():
+        oke, index = check_value(value)
+        if (oke and range_char(value[index])):
+            title1 = value[ : index]
+            dict_new[key] = title1
+            dict_new[value[index]] = value[index + 1 : ]            
+        else:
+            dict_new[key] = value
     
-#     # Tách câu hỏi và đáp án (dựa vào \nA hoặc \\A)
-#     if "\nA" in raw_text:
-#         parts = raw_text.split("\nA", 1)
-#     elif "\\A" in raw_text:
-#         parts = raw_text.split("\\A", 1)
-#     else:
-#         parts = [raw_text, ""]  # Nếu không có đáp án thì để trống
+    value_dict_tmp = dict(sorted(dict_new.items()))
+    return value_dict_tmp
 
-#     question = parts[0].strip()
-#     select = parts[1].strip()
 
-#     # Xử lý câu hỏi: bỏ số thứ tự nếu có
-#     # Ví dụ: 1. Câu hỏi, 2) Câu hỏi, 3 Câu hỏi
-#     question = re.sub(r"^\d+\s*[\.\)]?\s*", "", question)
-#     question = question.replace("\n", " ").strip()  # Xóa xuống dòng trong câu hỏi
-
-#     # Xử lý đáp án
-#     select = select.replace('\n', '').strip()  # Xóa hết xuống dòng cũ
-
-#     # Thêm xuống dòng trước B., C., D. để phân chia đáp án
-#     select = re.sub(r'(?=[BCD]\.)', r'\n', select)
-
-#     # Đảm bảo bắt đầu bằng A.
-#     if not select.startswith("A."):
-#         select = "A. " + select
-
-#     select = select.strip()
-
-#     return (question, select)
-
-def clean_question(raw_text : str) -> tuple:
-    # Tách phần câu hỏi và đáp án
-    if "\nA" in raw_text:
-        parts = raw_text.split("\nA", 1)
-    elif "\A" in raw_text:
-        parts = raw_text.split("\A", 1)
-    else:
-        parts = [raw_text, ""]
-
-    # Xử lý phần câu hỏi
-    question = parts[0].strip()
-    vtri = re.search(r"\d+", question)
-    
-    if vtri:
-        question = question[vtri.end():].strip()
-    question = question.replace(":", "")
-    question = question.replace("\n", "").strip()
-
-    # Xử lý phần đáp án
-    select = parts[1].strip()
-    select = select.replace('\n', '')
-    select = re.sub(r'(?=\n[BCD]\.)', r'\n', select)
-
-    select = select.strip()
-
-    if not select.startswith("A."):
-        select = "A" + select
-
-    return (question, select)
+def dict_to_text(data : Dict[str, str]) -> list[str]:
+    '''
+    data : dap an va noi dung cua dap an
+    '''
+    return [key + "." + value for key, value in data.items()]
 
 def solution(string : str) -> tuple:
+    '''
+    string : doan van ban dua vao
+    '''
     oke = True
     left = -1
     right = -1
@@ -95,6 +141,33 @@ def solution(string : str) -> tuple:
         string = string[ : -1]
 
     return (left,right)
+
+
+def clean_choice(options_raw : list[str]) -> Dict[str , str]:
+    '''
+    list_text : danh sach cac doan text cua dap an lua chon
+    '''
+    options = {}
+    current_key = None
+    current_text = []
+
+    for line in options_raw:
+        match = re.match(r"^([A-D])\.\s*$", line.strip())  # Chỉ chứa "A.", "B.",...
+        if match:
+            # Lưu đáp án trước đó nếu có
+            if current_key is not None:
+                options[current_key] = " ".join(current_text).strip()
+            # Bắt đầu đáp án mới
+            current_key = match.group(1)
+            current_text = []
+        elif current_key:
+            current_text.append(line.strip())
+
+    # Đừng quên lưu đáp án cuối cùng
+    if current_key is not None:
+        options[current_key] = " ".join(current_text).strip()
+    return options
+
 
 def parse_element(el):
     children = el.find_all(recursive=False)
@@ -163,9 +236,50 @@ def clean_mathml(mathml_string : str) -> str:
         return parse_element(soup.math)
     except ZeroDivisionError as e:
         return mathml_string
-    
+
+def binary_search(char : str, table_char : list[str], table_check : list[bool]) -> bool:
+    l = 0
+    r = len(table_char)
+    while(l < r):
+        mid = int((l + r) >> 1)
+        if (char == table_char[mid] and table_check[mid] == False):
+            table_check[mid] = True
+            return True
+        elif (char < table_char[mid]):
+            l = mid + 1
+        else:
+            r = mid - 1
+    return False
+
+def caculate(count : int, length : int) -> int:
+    if (length != 0):
+        return int(count / length) * 100
+    else:
+        return 0
+        
+
+def string_seach_character(string_clean : str, string_before : str) -> str:
+    length = len(string_clean)
+    vector_char = [string_clean[i] for i in range(len(string_clean))]
+    vector_char.sort()
+    table_check = [False for i in range(len(string_clean))]
+    for i in range(len(string_before) - length):
+        count_char = 0
+        sub_string = string_before[i : i + length]
+        for j in range(len(sub_string)):
+            if (binary_search(sub_string[i], vector_char, table_check) != -1):
+                count_char += 1
+        if (caculate(count_char, len(sub_string)) >= 60):
+            string_before = string_before.replace(string_before[i : i + length], "")
+            break
+        else:
+            continue
+    return string_before    
+
 def fix_spacing_and_comma(text : str) -> str:
-    
+    '''
+    text : doan text muon lam dep
+    '''    
     # 1. Sửa dấu phẩy giữa số thành dấu chấm (4 , 2 => 4.2)
     text = re.sub(r'(\d)\s*,\s*(\d)', r'\1.\2', text)
     
@@ -182,3 +296,21 @@ def fix_spacing_and_comma(text : str) -> str:
     # 5. Xóa khoảng trắng ở đầu/cuối
     text = text.strip()
     return text
+
+def convert_doube_string_from_string(text : str) -> str:
+    vector_text = text.split()
+    text_new = " ".join(vector_text)
+
+    vector_array = text_new.split(" ")
+
+    set_check = set()
+
+    vector_text_new = []
+
+    for i in vector_array:
+        if (i not in set_check):
+            vector_text_new.append(i)
+            set_check.add(i)
+
+    text_beautiful = " ".join(vector_text_new)
+    return text_beautiful
