08517291c49fc31a731497ff9a70ee461a039d91
PhamVanHung412004 <hp36780@gmail.com>
2025-05-03 01:14:06 +0700

    add file get_data_theory.py

--- Embedding_data/__init__.py
+++ Embedding_data/__init__.py
@@ -1,11 +1,12 @@
+
 class Get_Embedding:
-    def __init__(self,model,text):
+    def __init__(self,model,text : str):
         '''
         model: model dùng trong quá trình chuyển vecto
         text: dữ liệu đầu vào dưới dạng text
         '''
         self.model = model
-        self.text = text
+        self.text : str = text
 
     def use_model(self):
         text_vector = self.model.encode(self.text)
--- package/__init__.py
+++ package/__init__.py
@@ -1,6 +1,12 @@
+import os
+import re
 import numpy 
 import pandas
 import json
+import numpy 
+import faiss
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
 from pathlib import Path
 from pypdf import PdfReader
 from docx import Document
@@ -10,7 +16,4 @@ from sklearn.cluster import KMeans
 from yellowbrick.cluster import KElbowVisualizer
 from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
-import os
-import re
-from bs4 import BeautifulSoup
 
